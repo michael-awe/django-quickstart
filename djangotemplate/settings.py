ae09cc2e75647594916d05a166e7fbda94ed72af
Michael Awe <ionicawe@gmail.com>
2024-12-04 08:53:16 +0200

    Add NPM path to settings

--- djangotemplate/settings.py
+++ djangotemplate/settings.py
@@ -14,11 +14,15 @@ import os
 
 from pathlib import Path
 from dotenv import load_dotenv
+from shutil import which
+
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+NPM_BIN_PATH = which('npm')
 
 
 # Quick-start development settings - unsuitable for production
