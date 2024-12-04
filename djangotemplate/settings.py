0310938144a9e51c8c7e50c10a40ed6ea5c96774
Michael Awe <ionicawe@gmail.com>
2024-12-04 09:49:34 +0200

    Add postgres support

--- djangotemplate/settings.py
+++ djangotemplate/settings.py
@@ -32,7 +32,7 @@ NPM_BIN_PATH = which('npm')
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-25(@rn#gehs9i^-a9(v2m2$%bio6&r%%mx@2&=9m9r-wixk)6$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.getenv('DEBUG', 'True').lower() != 'false'
 
 ALLOWED_HOSTS = []
 
@@ -89,13 +89,29 @@ WSGI_APPLICATION = 'djangotemplate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+
+if DEBUG:
+    DATABASES = {
+        'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
 
+        'default': {
+
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+
+        }
+
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
